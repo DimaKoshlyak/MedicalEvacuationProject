f55a6e39f9c9b3e5f1fe4070c0cd36e6a5a12b14
Dima <admin.ptm@gmail.com>
2015-09-18 16:14:35 +0300

    Добавлены тест кейсы, добавлен @DatabaseSetup,  редактирование файла настроек

--- src/test/java/ua/kiev/dk/services/CrewPersistenceTest.java
+++ src/test/java/ua/kiev/dk/services/CrewPersistenceTest.java
@@ -1,6 +1,7 @@
 package ua.kiev.dk.services;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,29 +14,53 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import org.springframework.test.context.web.WebAppConfiguration;
 import ua.kiev.dk.config.PersistenceContextTest;
 import ua.kiev.dk.entities.Crew;
+import ua.kiev.dk.repositories.CrewRepository;
 
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
         TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class})
 @SpringApplicationConfiguration(classes = {PersistenceContextTest.class})
 @WebAppConfiguration
-public class PersistenceCrewTest {
+@DatabaseSetup(value = "classpath:db.xml")
+public class CrewPersistenceTest {
     @Autowired
     private CrewManager crewManager;
+    @Autowired
+    private CrewRepository crewRepository;
 
     @Test
-    public void findAllCrews() {
+    public void findAllCrewsTest() {
         Collection<Crew> crews = crewManager.listCrews();
-        assertEquals(4, crews.size());
+        assertEquals(5, crews.size());
     }
 
     @Test
-    public void findOneCrew() {
-        assertEquals("ASAP1", crewManager.returnCrewFromName("ASAP1").getCrewName());
+    public void findOneCrewTest() {
+        assertEquals("ATO-01", crewManager.returnCrewFromName("ATO-01").getCrewName());
+    }
+
+    @Test
+    public void addCrewTest() {
+        Crew crew = new Crew();
+        crew.setCrewName("ASAP Test");
+        crew.setMember1Name("Test Name1");
+        crew.setMember2Name("Test Name2");
+        crew.setCar("Test Car1");
+        crew.setReanimation("true");
+        crewManager.addCrew(crew);
+        Collection<Crew> crews = crewManager.listCrews();
+        assertEquals(6, crews.size());
+    }
+
+    @Test
+    public void deleteCrewTest() {
+        crewManager.deleteCrew(5L);
+        assertNull(crewRepository.findOne(5L));
     }
 }
 
